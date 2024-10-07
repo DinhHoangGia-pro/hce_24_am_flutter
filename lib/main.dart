95446ea13af88c6c1eb8b22a4fcf987ad711a4a9
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-07 08:01:23 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,19 @@ void main() {
             appBar: AppBar(
                 title: Image.network(
                     'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060')),
-            body: Column(
-              children: [
-                const ButtonSection(),
-                CongHaiSo_Stateful(),
-                CongHaiSo_Stateful(),
-              ],
+            body: Container(
+              margin: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Color(0xffb53f7a),
+              ),
+              child: Column(
+                children: [
+                  const ButtonSection(),
+                  CongHaiSo_Stateful(),
+                  CongHaiSo_Stateful(),
+                ],
+              ),
             )),
       )));
 }
